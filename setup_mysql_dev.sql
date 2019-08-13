f6f4dd260037d6dc5d210ea7a0df8c956b36153c
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-08-13 22:39:52 +0000

    [fix] create user

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,6 @@
 -- creates the database hbnb_dev_db and the user hbnb_dev
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER iF NOT EXISTS hbnb_dev@localhost;
-GRANT ALL PRIVILEGES ON hbtn_dev_db TO hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_p\
+wd';
+GRANT ALL PRIVILEGES ON hbtn_dev_db TO hbnb_dev@localhost;
+GRANT SELECT ON performance_schema TO hbnb_dev@localhost;
