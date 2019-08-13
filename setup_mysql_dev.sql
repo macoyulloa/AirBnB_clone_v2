5323c6cdc02e76a2c19455f684872d0fab9a6707
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-08-13 22:17:42 +0000

    [add] comment

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+-- creates the database hbnb_dev_db and the user hbnb_dev
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER iF NOT EXISTS hbnb_dev@localhost;
 GRANT ALL PRIVILEGES ON hbtn_dev_db TO hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
