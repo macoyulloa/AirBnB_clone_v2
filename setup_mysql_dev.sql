706abe61699e8491243fe48dae9f7e9c415374c7
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-08-13 22:12:54 +0000

    [setup] first user and database

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER iF NOT EXISTS hbnb_dev@localhost;
+GRANT ALL PRIVILEGES ON hbtn_dev_db TO hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
