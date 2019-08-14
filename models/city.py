e955850401ea931fe5b827be0aa18912bcef7dbc
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-08-14 17:22:13 +0000

    [change] to sqalchemy

--- models/city.py
+++ models/city.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""This is the city class"""
-from models.base_model import BaseModel
+'class definition of a State and an instance Base = declarative_base()'
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from models.base_model import BaseModel, Base
 
 
-class City(BaseModel):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
-    """
-    state_id = ""
-    name = ""
+class City(Basemodel, Base):
+    ' class city inherits from Basemodel and base'
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
