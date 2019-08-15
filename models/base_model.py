c5c6033d7d4f2c919ab5e8cf23f91141acb18acc
macoyulloa <malejacoy@hotmail.com>
2019-08-15 21:19:54 +0000

    correctgion

--- models/base_model.py
+++ models/base_model.py
@@ -44,6 +44,9 @@ class BaseModel():
         Return:
             returns a string of class name, id, and dictionary
         """
+        tmp = self.__dict__
+        if "_sa_instance_state" in tmp.keys():
+            del tmp['_sa_instance_state']
         return "[{}] ({}) {}".format(
             type(self).__name__, self.id, self.__dict__)
 
