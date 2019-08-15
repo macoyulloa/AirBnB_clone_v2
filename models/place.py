29bf19de86ed4177e2b4bfc1bc64c8b11ac9a19d
macoyulloa <malejacoy@hotmail.com>
2019-08-15 20:53:15 +0000

    attribute correction

--- models/place.py
+++ models/place.py
@@ -2,7 +2,7 @@
 """This is the place class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, Float, String, ForeignKey
-
+import models
 
 class Place(BaseModel, Base):
     """This is the class for Place
--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-
+import models
 
 class User(BaseModel, Base):
     """This is the class for user
@@ -19,4 +19,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship("Place", cascade="delete", backref="user")
+    places = relationship("Place", backref="user")
