bc49325dd80123ada643a1cd22d07641284e17ec
macoyulloa <malejacoy@hotmail.com>
2019-08-16 02:04:39 +0000

    Revert "[feature] add a feature to interconect it amenity and place throw a table"
    This reverts commit 78f2f78360dd1dd9461f6a2958c96b1448b177ab.

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.orm import relationship
-from models.place import place_amenity
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
+class Amenity(BaseModel):
     """This is the class for Amenity
     Attributes:
         name: input name
     """
-
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity,
-                                   backref='amenities')
+    name = ""
