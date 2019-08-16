78f2f78360dd1dd9461f6a2958c96b1448b177ab
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-08-16 01:40:55 +0000

    [feature] add a feature to interconect it amenity and place throw a table

--- models/place.py
+++ models/place.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 """This is the place class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, Float, String, ForeignKey
+from sqlalchemy import Column, Integer, Float, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 import models
+from sqlalchemy import *
+
+
+place_amenity = Table('place_amenity', metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             nullable=False))
+
+metadata = Base.metadata
 
 
 class Place(BaseModel, Base):
@@ -16,7 +27,7 @@ class Place(BaseModel, Base):
         number_rooms: number of room in int
         number_bathrooms: number of bathrooms in int
         max_guest: maximum guest in int
-        price_by_night:: pice for a staying in int
+            price_by_night:: pice for a staying in int
         latitude: latitude in flaot
         longitude: longitude in float
         amenity_ids: list of Amenity ids
@@ -35,3 +46,26 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place")
+    amenity_ids = []
+    amenities = relationship("Amenity", secondary="place_amenity",
+                             viewonly=False, backref="place_amenities")
+
+
+    @property
+    def amenities(self, obj):
+        """returns the list of Amenity instances"""
+        amenitys_list = ()
+        dict_amenitys = models.storage.all(models.Amenity)
+        for key, value in dict_amenitys.items():
+            for search_id in self.anemitys_ids:
+                if value.id == search_id
+                amenitys_list.append(value)
+        return(amenitys_list)
+
+    @amenities.setter
+    def amenities(self, obj):
+        """append method for adding an Amenity.id to attribute amenity_ids"""
+        if (type(obj).__Classname__) == Amenity:
+            self.amenity_ids.append(obj.id)
+
+
