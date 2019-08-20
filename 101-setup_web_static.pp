b1b35489080c70955a351b92d4a97ddcee69a27b
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-08-20 14:59:03 +0000

    advanced task

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,20 @@
+# set custom header http
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update',
+}
+->package { 'install nginx':
+  ensure  => 'present',
+  require => Exec['apt-get update'],
+}
+->file_line { 'header http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+->service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
+->exec { 'service nginx restart':
+  command => '/usr/sbin/service nginx restart',
+}
